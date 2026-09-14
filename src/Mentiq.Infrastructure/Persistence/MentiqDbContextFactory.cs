27fc6d97fcb7dbc6cc327ea0caf9d23fcbac327b
ninngiorgadze <ninngiorgadze@gmail.com>
2026-09-14 18:40:34 +0400

    Minor

--- src/Mentiq.Infrastructure/Persistence/MentiqDbContextFactory.cs
+++ src/Mentiq.Infrastructure/Persistence/MentiqDbContextFactory.cs
@@ -12,9 +12,11 @@ namespace Mentiq.Infrastructure.Persistence;
 /// </summary>
 public sealed class MentiqDbContextFactory : IDesignTimeDbContextFactory<MentiqDbContext>
 {
-    private const string FallbackConnectionString =
-        "Server=localhost;Database=MentiqDb;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=true;Encrypt=False";
+    // private const string FallbackConnectionString =
+    //     "Server=localhost;Database=MentiqDb;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=true;Encrypt=False";
 
+    private const string FallbackConnectionString =
+        "Server=db68276.public.databaseasp.net; Database=db68276; User Id=db68276; Password=G%p32J!yTc9=; Encrypt=False; MultipleActiveResultSets=True;";
     public MentiqDbContext CreateDbContext(string[] args)
     {
         var connectionString =
