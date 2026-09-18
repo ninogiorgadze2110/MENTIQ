9da4d9b3f2c557b9c1d02da90bb799f78b2ce83c
ninngiorgadze <ninngiorgadze@gmail.com>
2026-09-18 16:49:19 +0400

    BUIILD

---
+++ src/Mentiq.Api/wwwroot/chunk-AXKTLJVK.js
@@ -0,0 +1,1 @@
+import{M as r,za as t}from"./chunk-UWUEOP3X.js";var s=class e{session=t(null);set(n){this.session.set(n)}static \u0275fac=function(o){return new(o||e)};static \u0275prov=r({token:e,factory:e.\u0275fac,providedIn:"root"})};export{s as a};
